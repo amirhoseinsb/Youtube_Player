84f12b16c60dfcabe81184f99e28f3f54bbe5f41
amirhosein sohrabi <amirhoseinsohrabi.official@gmail.com>
2022-07-29 18:53:45 +0430

    edit player

--- player.py
+++ player.py
@@ -1,11 +1,22 @@
-import playsound
+from playsound import playsound
 from banner import PLAYER
+import multiprocessing
 class player:
     def __init__(self):
         print(PLAYER)
-        self.link = input('\nPlease Enter The Link :')
+        self.link = input('Please Enter The Link :')
+        if self.link.lower() == 'x':
+            exit()
     def play_sound(self):
-        playsound.playsound(self.link)
+        self.music = multiprocessing.Process(target=playsound, args=(self.link,))
+        self.music.start()
+    def change_music(self):
+        input('\nPress Enter For Play Next Music !')
+        self.music.terminate()
+def run():
+    music = player()
+    music.play_sound()
+    music.change_music()
 
-music = player()
-music.play_sound()
+while True:
+    run()
